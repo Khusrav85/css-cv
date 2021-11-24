3d764a834f557985f9d3ccfc22722893da7ae55d
Khusrav85 <94967318+Khusrav85@users.noreply.github.com>
2021-11-24 14:42:37 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Milana cv</title>
+    <link  href="css/style.css" rel="stylesheet"/>
+</head>
+
+<body>
+
+<div class="bloc">
+    <div class="info">
+    <img src="img/Photo%20Safieva%20Milana.jpg" alt="тут моя фотка" class="avatar">
+        <div class="info-text"></div>
+
+    <h1>Сафиева Милана</h1>
+
+    <p>Я люблю тратить деньги></p>
+    <p>Я люблю путешествовать, заниматься спортом и зарабатывать много денег</p>
+
+        <h2>Мои социальные сети</h2>
+        <a href="https://instagram.com/milana_safieva"
+           target="_blank">Instagram</a>
+        |
+        <a href="https://t.me/Milana160789"
+           target="_blank">Telegram</a>
+    </div>
+
+
+    <div class="card bg-yellow">
+    <h3>Мои хобби</h3>
+        <ol class="list">
+            <li>Путешествия</li>
+            <li>Кино</li>
+            <li>Музыка</li>
+            <li>Делать мозги мужу</li>
+        </ol>
+
+    </div>
+
+    <div class="card bg-green">
+        <h4>Опыт работы</h4>
+        <table class="table">
+            <tr>
+                <td>Евразия</td>
+                <td>Менеджер</td>
+                <td>2007-2010</td>
+            </tr>
+            <tr>
+                <td>СушиВок</td>
+                <td>Оператор кол-центр</td>
+                <td>2010-2012</td>
+            </tr>
+            <tr>
+                <td>Сакура</td>
+                <td>Ассистент операционого директора</td>
+                <td>2014-2021</td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="card bg-red">
+        <h5>Навыки</h5>
+
+
+    </div>
+
+    <div class="contact">
+        <h6>Связаться со мной</h6>
+
+        <form action="/">
+
+
+        <div class="form-group">
+
+        <label for="name">Ваше имя:</label>
+        <input type="text" id="name" placeholder="Введите ваше имя...">
+
+        </div>
+
+        <div class="form-group">
+
+        <label for="email">Ваш email:</label>
+        <input type="email" id="email" placeholder="Введите email...">
+        <label for="text">Ваше сообщение:</label>
+
+        </div>
+
+
+            <div class="form-group">
+        <textarea id="text" rows="2" cols="15"></textarea>
+
+            <input type="submit" value="Связаться">
+            </div>
+
+        </form>
+
+    </div>
+</div>
+
+
+
+<script src="js/script.js"></script>
+
+</div>
+</body>
+</html>
+
